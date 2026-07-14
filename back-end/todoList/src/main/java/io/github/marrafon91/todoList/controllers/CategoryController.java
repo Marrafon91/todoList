7475292cd5abefd6b58a58beeaf47afb5648e148
Guilherme <guilherme.marrafon1@gmail.com>
2026-07-14 07:15:26 -0300

    Categorias end-point feito

---
+++ back-end/todoList/src/main/java/io/github/marrafon91/todoList/controllers/CategoryController.java
@@ -0,0 +1,33 @@
+package io.github.marrafon91.todoList.controllers;
+
+import io.github.marrafon91.todoList.dtos.CategoryDTO;
+import io.github.marrafon91.todoList.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> result = categoryService.findAllCategories();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO dto = categoryService.findCategoryById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+}
