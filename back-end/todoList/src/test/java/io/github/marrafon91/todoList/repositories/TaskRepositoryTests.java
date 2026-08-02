435503780856fa51a8e480ebc7b506dae5f3969f
Guilherme <guilherme.marrafon1@gmail.com>
2026-08-02 20:04:40 -0300

    Testando todo os filtros da contulta com specification

--- back-end/todoList/src/test/java/io/github/marrafon91/todoList/repositories/TaskRepositoryTests.java
+++ back-end/todoList/src/test/java/io/github/marrafon91/todoList/repositories/TaskRepositoryTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import io.github.marrafon91.todoList.specifications.TaskSpecification;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,6 +29,7 @@ public class TaskRepositoryTests {
 
     @BeforeEach
     public void setup() {
+
         category = new Category();
         category.setName("Trabalho");
         category.setColor("#2563EB");
@@ -43,6 +46,7 @@ public class TaskRepositoryTests {
 
         task = taskRepository.save(task);
     }
+
     @Test
     public void findAllShouldReturnTasksWithCategory() {
         List<Task> result = taskRepository.findAll();
@@ -61,5 +65,53 @@ public class TaskRepositoryTests {
         assertEquals(category.getId(), savedTask.getCategory().getId());
         assertEquals("Trabalho", savedTask.getCategory().getName());
         assertEquals("#2563EB", savedTask.getCategory().getColor());
+    }
+
+    @Test
+    public void findAllShouldReturnTasksFilteredByTitleWithCategory() {
+
+        Specification<Task> specification = TaskSpecification.titleContains("Java");
+
+        List<Task> result = taskRepository.findAll(specification);
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertTrue(result.stream()
+                .allMatch(task -> task.getTitle().toLowerCase().contains("java")));
+
+        Task foundTask = result.stream()
+                .filter(task -> task.getTitle().equalsIgnoreCase("Estudar Java"))
+                .findFirst()
+                .orElseThrow();
+
+        assertNotNull(foundTask.getCategory());
+        assertEquals("Trabalho", foundTask.getCategory().getName());
+        assertEquals("#2563EB", foundTask.getCategory().getColor());
+    }
+
+    @Test
+    public void findAllShouldReturnTasksFilteredByMultipleSpecifications() {
+
+        Specification<Task> specification =
+                Specification.where(TaskSpecification.titleContains("Java"))
+                        .and(TaskSpecification.done(false))
+                        .and(TaskSpecification.priority(Priority.HIGH))
+                        .and(TaskSpecification.category(category.getId()));
+
+        List<Task> result = taskRepository.findAll(specification);
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+
+        Task foundTask = result.getFirst();
+
+        assertEquals("Estudar Java", foundTask.getTitle());
+        assertFalse(foundTask.isDone());
+        assertEquals(Priority.HIGH, foundTask.getPriority());
+        assertNotNull(foundTask.getCategory());
+        assertEquals(category.getId(), foundTask.getCategory().getId());
+        assertEquals("Trabalho", foundTask.getCategory().getName());
+        assertEquals("#2563EB", foundTask.getCategory().getColor());
     }
 }
