3f3a12c129626f7898d06dd0015dab764152e4fe
Guilherme <guilherme.marrafon1@gmail.com>
2026-08-03 09:29:16 -0300

    Fazendo teste na camada user

---
+++ back-end/todoList/src/test/java/io/github/marrafon91/todoList/repositories/UserRepositoryTests.java
@@ -0,0 +1,36 @@
+package io.github.marrafon91.todoList.repositories;
+
+import io.github.marrafon91.todoList.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findByNameShouldReturnUsersWhenNameMatches() {
+
+        List<User> result = userRepository.findByName("Guilherme");
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+
+        User foundUser = result.stream()
+                .filter(user -> user.getName().equals("Guilherme"))
+                .findFirst()
+                .orElseThrow();
+
+        assertEquals("Guilherme", foundUser.getName());
+        assertEquals("guilherme@gmail.com", foundUser.getEmail());
+        assertEquals("123456", foundUser.getPassword());
+    }
+}
