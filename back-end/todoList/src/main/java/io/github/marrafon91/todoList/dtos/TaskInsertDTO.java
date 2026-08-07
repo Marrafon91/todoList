c12c04ac2cf7213c58b4a97bf3a816e2a54626a3
Guilherme <guilherme.marrafon1@gmail.com>
2026-08-07 08:06:40 -0300

    Swagger nos DTOS insert e update Task

--- back-end/todoList/src/main/java/io/github/marrafon91/todoList/dtos/TaskInsertDTO.java
+++ back-end/todoList/src/main/java/io/github/marrafon91/todoList/dtos/TaskInsertDTO.java
@@ -1,6 +1,7 @@
 package io.github.marrafon91.todoList.dtos;
 
 import io.github.marrafon91.todoList.entities.Priority;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,14 +9,40 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public record TaskInsertDTO(
+
+        @Schema(
+                description = "Título da tarefa",
+                example = "Estudar Spring Boot",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
         @Size(min = 3, max = 100, message = "O título é obrigatório, deve ter de 3 a 100 caracteres")
         String title,
+
+        @Schema(
+                description = "Descrição da tarefa",
+                example = "Revisar Specifications e JPA"
+        )
         @Size(min = 10, max = 500, message = "A Descrição deve ter de 10 a 500 caracteres")
         String description,
+
+        @Schema(
+                description = "Prioridade da tarefa",
+                example = "HIGH"
+        )
         @NotNull(message = "A prioridade é obrigatória")
         Priority priority,
+
+        @Schema(
+                description = "ID da categoria",
+                example = "2"
+        )
         @NotNull(message = "A categoria é obrigatória")
         Long categoryId,
+
+        @Schema(
+                description = "Data de vencimento",
+                example = "2027-08-15"
+        )
         @FutureOrPresent(message = "A data de vencimento é obrigatória, deve ser uma data futura")
         LocalDate dueDate
 ) {
--- back-end/todoList/src/main/java/io/github/marrafon91/todoList/dtos/TaskUpdateDTO.java
+++ back-end/todoList/src/main/java/io/github/marrafon91/todoList/dtos/TaskUpdateDTO.java
@@ -1,6 +1,7 @@
 package io.github.marrafon91.todoList.dtos;
 
 import io.github.marrafon91.todoList.entities.Priority;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,19 +9,41 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public record TaskUpdateDTO(
+
+        @Schema(
+                description = "Título da tarefa",
+                example = "Estudar Spring Boot",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
         @NotBlank(message = "O título é obrigatório")
         @Size(min = 3, max = 100)
         String title,
 
+        @Schema(
+                description = "Descrição da tarefa",
+                example = "Revisar Specifications e JPA"
+        )
         @Size(min = 10 ,max = 500, message = "tamanho deve ser entre 10 e 500")
         String description,
 
+        @Schema(
+                description = "Prioridade da tarefa",
+                example = "HIGH"
+        )
         @NotNull(message = "A prioridade é obrigatória")
         Priority priority,
 
+        @Schema(
+                description = "ID da categoria",
+                example = "2"
+        )
         @NotNull(message = "A categoria é obrigatória")
         Long categoryId,
 
+        @Schema(
+                description = "Data de vencimento",
+                example = "2027-08-15"
+        )
         @NotNull(message = "A data de vencimento é obrigatória")
         LocalDate dueDate
 
